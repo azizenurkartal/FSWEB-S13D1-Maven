555adcfa58430f2921e4d86186f5790f5f706fa8
Azize Nur Kartal <azizenurkartal@Azize-MacBook-Air.local>
2026-07-07 19:45:24 +0300

    Tüm metodlar tamamlandı, testler geçti

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,40 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+    // Havlayan Köpek
+    public static boolean shouldWakeUp(boolean barking, int clock) {
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        return barking && (clock < 8 || clock >= 20);
     }
 
+    // Yaş Tespiti
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
+    // Oyuncu Kedi
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upperLimit = isSummer ? 45 : 35;
+        return temp >= 25 && temp <= upperLimit;
     }
 
+    // Dikdörtgen Alanı
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
     }
 
+    // Daire Alanı
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
